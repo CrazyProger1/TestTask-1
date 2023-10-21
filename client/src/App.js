57e62b4e791355889c7dd1fbc3e3a3ffc47dd0fd
CrazyProger1 <crazyproger1@gmail.com>
2023-10-21 12:02:35 +0300

    Add: empty react project

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UserTable from "./components/UserTable";
-import UserModal from "./components/UserModal";
 
 function App() {
     return (
         <div className="App">
-            <UserTable/>
+
         </div>
     );
 }
